c806a182bff50bb073a4ce8e056a2f8d7ce030af
florian <bessonflorian42@yahoo.fr>
2025-11-29 12:10:57 +0100

    argocd basic install

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -12,11 +12,17 @@ check_binary() {
 check_binary k3d
 check_binary kubectl
 
-
-k3d cluster create -p 8080:80@loadbalancer
+k3d cluster create fbessonS -p "8080:80@loadbalancer"
 
 kubectl create namespace argocd
 kubectl create namespace dev
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl wait --for=condition=Ready -n argocd --all pod --timeout=300s
+kubectl port-forward svc/argocd-server -n argocd 8081:443 &
+disown
+kubectl -n argocd get secret argocd-initial-admin-secret \
+  -o jsonpath="{.data.password}" | base64 --decode
+echo " "
 
 neofetch
 if command -v figlet &>/dev/null; then
