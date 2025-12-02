c74ab38270dc3875dc10a6cd52e22614cd33eb44
florian <bessonflorian42@yahoo.fr>
2025-12-02 17:17:31 +0100

    add port forwarding provisionning

--- p3/scripts/port-forward.sh
+++ p3/scripts/port-forward.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ $(whoami) != root ]; then
+# always run as root
+if [ "$(whoami)" != "root" ]; then
     exec sudo bash "$0"
 fi
 
-(
-  while true; do
-    kubectl port-forward svc/argocd-server -n argocd --address 0.0.0.0 8080:443 >/dev/null 2>&1
+while true; do
+    echo "[ArgoCD] starting port-forward 8080 -> 80 ..."
+    kubectl port-forward svc/argocd-server -n argocd 8080:80 --address 0.0.0.0 >/dev/null 2>&1
+    echo "[ArgoCD] port-forward crashed, restarting in 2 sec..."
     sleep 2
-  done
-) &
+done
