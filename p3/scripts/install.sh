6d0fa287db92e173cc371b6fb92410a93757065e
florian <bessonflorian42@yahoo.fr>
2025-12-12 16:38:38 +0100

    need opti/grok argocd config et usage

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -10,7 +10,7 @@ usermod -aG docker vagrant
 #install k3d via curl
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-# install kubectl
+#k3d doesnt install kubectl by default, so we install it
 curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
